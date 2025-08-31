917cb221ad128fc37ab12bea0b1d9bf6081a7b8f
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-08-31 18:43:43 +0900

    1.21

--- src/main/java/mypals/ml/mixin/LevelRendererMixin.java
+++ src/main/java/mypals/ml/mixin/LevelRendererMixin.java
@@ -1,16 +1,13 @@
 package mypals.ml.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.moulberry.flashback.Flashback;
 import com.moulberry.flashback.state.EditorState;
 import com.moulberry.flashback.state.EditorStateManager;
 import com.moulberry.flashback.visuals.ReplayVisuals;
-import mypals.ml.CompatibleIrisSkyOverride;
 import net.irisshaders.iris.api.v0.IrisApi;
 import net.minecraft.client.render.*;
-import net.minecraft.client.util.Handle;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,50 +19,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
 
-@Mixin(value = WorldRenderer.class, priority = 900)
+@Mixin(value = WorldRenderer.class, priority = 2000)
 public class LevelRendererMixin {
     @Shadow @Final private BufferBuilderStorage bufferBuilders;
 
 
 
-    @Inject(method = {"method_62215"}, require = 1,
-            at = @At(value = "INVOKE",
-                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFog" +
-                            "(Lnet/minecraft/client/render/Fog;)V", shift = At.Shift.AFTER),
-            cancellable = true)
-    private void renderCustomSky(CallbackInfo ci) {
+    @Inject(method = {"render"}, require = 1,allow = 1,
+    at = @At(value = "INVOKE",shift = At.Shift.BEFORE,
+                    target = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V"))
+    private void renderCustomSky(RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
         EditorState editorState = EditorStateManager.getCurrent();
         if (editorState != null) {
             ReplayVisuals visuals = editorState.replayVisuals;
             //CompatibleIrisSkyOverride.LOGGER.info("Checking if sky disc should be rendered: " + visuals.renderSky);
             if(!visuals.renderSky && IrisApi.getInstance().isShaderPackInUse()){
                 render(visuals.skyColour);
-                ci.cancel();
             }
         }
     }
-    @Inject(method = "method_62212", require = 1,
-            at = @At(value = "INVOKE",
-                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFog" +
-                            "(Lnet/minecraft/client/render/Fog;)V", shift = At.Shift.AFTER),
-            cancellable = true)
-    private void renderCustomSky2(CallbackInfo ci) {
-        EditorState editorState = EditorStateManager.getCurrent();
-        if (editorState != null) {
-            ReplayVisuals visuals = editorState.replayVisuals;
-            //CompatibleIrisSkyOverride.LOGGER.info("Checking if sky disc should be rendered: " + visuals.renderSky);
-            if(!visuals.renderSky && IrisApi.getInstance().isShaderPackInUse()){
-                render(visuals.skyColour);
-                //ci.cancel();
-            }
-        }
-    }
+
     @Unique
     public void render(float[] skyColour) {
         float a = 1.0F;
         RenderSystem.setShaderColor(skyColour[0], skyColour[1], skyColour[2], a);
-        VertexConsumer vertexConsumer = this.bufferBuilders.getEntityVertexConsumers().getBuffer(RenderLayer.getSky());
-
+        BufferBuilder bufferBuilder = Tessellator.getInstance()
+                .begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
         for (int i = 0; i < 6; i++) {
             Matrix4f matrix4f = new Matrix4f();
             switch (i) {
@@ -85,16 +64,16 @@ public class LevelRendererMixin {
                     matrix4f.rotationZ((float) (-Math.PI / 2));
             }
 
-            vertexConsumer.vertex(matrix4f, -100.0F, -100.0F, -100.0F)
+            bufferBuilder.vertex(matrix4f, -100.0F, -100.0F, -100.0F)
                     .color(skyColour[0], skyColour[1], skyColour[2], a);
-            vertexConsumer.vertex(matrix4f, -100.0F, -100.0F, 100.0F)
+            bufferBuilder.vertex(matrix4f, -100.0F, -100.0F, 100.0F)
                     .color(skyColour[0], skyColour[1], skyColour[2], a);
-            vertexConsumer.vertex(matrix4f, 100.0F, -100.0F, 100.0F)
+            bufferBuilder.vertex(matrix4f, 100.0F, -100.0F, 100.0F)
                     .color(skyColour[0], skyColour[1], skyColour[2], a);
-            vertexConsumer.vertex(matrix4f, 100.0F, -100.0F, -100.0F)
+            bufferBuilder.vertex(matrix4f, 100.0F, -100.0F, -100.0F)
                     .color(skyColour[0], skyColour[1], skyColour[2], a);
         }
-        this.bufferBuilders.getEntityVertexConsumers().draw(RenderLayer.getSky());
+        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
