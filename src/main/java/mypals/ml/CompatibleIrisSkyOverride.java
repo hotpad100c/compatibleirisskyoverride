7beb1678608ad1d104e4030d76ac798fc316e252
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-10-04 20:25:17 +0900

    1.21.4

--- src/main/java/mypals/ml/CompatibleIrisSkyOverride.java
+++ src/main/java/mypals/ml/CompatibleIrisSkyOverride.java
@@ -1,9 +1,14 @@
 package mypals.ml;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.moulberry.flashback.state.EditorState;
 import com.moulberry.flashback.state.EditorStateManager;
 import com.moulberry.flashback.visuals.ReplayVisuals;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.BufferBuilderStorage;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import org.joml.Matrix4f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,5 +27,41 @@ public class CompatibleIrisSkyOverride implements ModInitializer {
 		// Proceed with mild caution.
 
 		//LOGGER.info("Hello Fabric world!");
+	}
+	public static void render(BufferBuilderStorage bufferBuilderStorage,float[] skyColour) {
+		float a = 1.0F;
+		RenderSystem.setShaderColor(skyColour[0], skyColour[1], skyColour[2], a);
+		VertexConsumer vertexConsumer = bufferBuilderStorage.getEntityVertexConsumers().getBuffer(RenderLayer.getSky());
+
+		for (int i = 0; i < 6; i++) {
+			Matrix4f matrix4f = new Matrix4f();
+			switch (i) {
+				case 1:
+					matrix4f.rotationX((float) (Math.PI / 2));
+					break;
+				case 2:
+					matrix4f.rotationX((float) (-Math.PI / 2));
+					break;
+				case 3:
+					matrix4f.rotationX((float) Math.PI);
+					break;
+				case 4:
+					matrix4f.rotationZ((float) (Math.PI / 2));
+					break;
+				case 5:
+					matrix4f.rotationZ((float) (-Math.PI / 2));
+			}
+
+			vertexConsumer.vertex(matrix4f, -100.0F, -100.0F, -100.0F)
+					.color(skyColour[0], skyColour[1], skyColour[2], a);
+			vertexConsumer.vertex(matrix4f, -100.0F, -100.0F, 100.0F)
+					.color(skyColour[0], skyColour[1], skyColour[2], a);
+			vertexConsumer.vertex(matrix4f, 100.0F, -100.0F, 100.0F)
+					.color(skyColour[0], skyColour[1], skyColour[2], a);
+			vertexConsumer.vertex(matrix4f, 100.0F, -100.0F, -100.0F)
+					.color(skyColour[0], skyColour[1], skyColour[2], a);
+		}
+		bufferBuilderStorage.getEntityVertexConsumers().draw(RenderLayer.getSky());
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 	}
 }
--- src/main/java/mypals/ml/mixin/LevelRendererMixin.java
+++ src/main/java/mypals/ml/mixin/LevelRendererMixin.java
@@ -22,6 +22,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
 
+import static mypals.ml.CompatibleIrisSkyOverride.render;
+
 @Mixin(value = WorldRenderer.class, priority = 900)
 public class LevelRendererMixin {
     @Shadow @Final private BufferBuilderStorage bufferBuilders;
@@ -39,62 +41,9 @@ public class LevelRendererMixin {
             ReplayVisuals visuals = editorState.replayVisuals;
             //CompatibleIrisSkyOverride.LOGGER.info("Checking if sky disc should be rendered: " + visuals.renderSky);
             if(!visuals.renderSky && IrisApi.getInstance().isShaderPackInUse()){
-                render(visuals.skyColour);
+                render(this.bufferBuilders, visuals.skyColour);
                 ci.cancel();
             }
         }
-    }
-    @Inject(method = "method_62212", require = 1,
-            at = @At(value = "INVOKE",
-                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFog" +
-                            "(Lnet/minecraft/client/render/Fog;)V", shift = At.Shift.AFTER),
-            cancellable = true)
-    private void renderCustomSky2(CallbackInfo ci) {
-        EditorState editorState = EditorStateManager.getCurrent();
-        if (editorState != null) {
-            ReplayVisuals visuals = editorState.replayVisuals;
-            //CompatibleIrisSkyOverride.LOGGER.info("Checking if sky disc should be rendered: " + visuals.renderSky);
-            if(!visuals.renderSky && IrisApi.getInstance().isShaderPackInUse()){
-                render(visuals.skyColour);
-                //ci.cancel();
-            }
-        }
-    }
-    @Unique
-    public void render(float[] skyColour) {
-        float a = 1.0F;
-        RenderSystem.setShaderColor(skyColour[0], skyColour[1], skyColour[2], a);
-        VertexConsumer vertexConsumer = this.bufferBuilders.getEntityVertexConsumers().getBuffer(RenderLayer.getSky());
-
-        for (int i = 0; i < 6; i++) {
-            Matrix4f matrix4f = new Matrix4f();
-            switch (i) {
-                case 1:
-                    matrix4f.rotationX((float) (Math.PI / 2));
-                    break;
-                case 2:
-                    matrix4f.rotationX((float) (-Math.PI / 2));
-                    break;
-                case 3:
-                    matrix4f.rotationX((float) Math.PI);
-                    break;
-                case 4:
-                    matrix4f.rotationZ((float) (Math.PI / 2));
-                    break;
-                case 5:
-                    matrix4f.rotationZ((float) (-Math.PI / 2));
-            }
-
-            vertexConsumer.vertex(matrix4f, -100.0F, -100.0F, -100.0F)
-                    .color(skyColour[0], skyColour[1], skyColour[2], a);
-            vertexConsumer.vertex(matrix4f, -100.0F, -100.0F, 100.0F)
-                    .color(skyColour[0], skyColour[1], skyColour[2], a);
-            vertexConsumer.vertex(matrix4f, 100.0F, -100.0F, 100.0F)
-                    .color(skyColour[0], skyColour[1], skyColour[2], a);
-            vertexConsumer.vertex(matrix4f, 100.0F, -100.0F, -100.0F)
-                    .color(skyColour[0], skyColour[1], skyColour[2], a);
-        }
-        this.bufferBuilders.getEntityVertexConsumers().draw(RenderLayer.getSky());
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
